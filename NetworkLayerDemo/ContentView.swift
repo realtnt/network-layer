cfc5f9b0288307cd39853f48720fbab47be906df
realtnt <theo@thetaphoto.co.uk>
2023-10-03 08:25:34 +0100

    Initial commit

---
+++ NetworkLayerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkLayerDemo
+//
+//  Created by Theo Ntogiakos on 02/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkLayerDemo/NetworkLayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerDemoApp.swift
+//  NetworkLayerDemo
+//
+//  Created by Theo Ntogiakos on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
