f06d0b8478f3678d3e0c0e2fa1c1503ddaa257e3
realtnt <theo@thetaphoto.co.uk>
2023-10-06 23:42:43 +0100

    SwiftUI

--- NetworkLayerDemo/ContentView.swift
+++ NetworkLayerDemo/ContentView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        HStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .resizable()
+                .frame(width: 100, height: 100)
+            VStack(alignment: .leading) {
+                Text("ID:")
+                Text("Title:")
+                Text("First Name:")
+                Text("Last Name:")
+                Text("Email:")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+//let id: String?
+//let title: String?
+//let firstName: String?
+//let lastName: String?
+//let email: String?
+//let picture: String?
